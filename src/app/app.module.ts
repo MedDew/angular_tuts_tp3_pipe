20eddc03854ba7f506d8387ca83771d08f95f479
Med <mehdi.gacem@versusmind.eu>
2018-10-29 15:46:46 +0100

    Global application module : add parent carsmodule + add HttpClientModule + add API_CONFIG_BASE_URL class to be injected in car service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { API_CONFIG_BASE_URL } from './app.config';
+// import { CarsComponent } from './cars/cars.component';
+import { CarsModule } from './cars/cars.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //CarsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    CarsModule,
   ],
-  providers: [],
+  providers: [{ provide: API_CONFIG_BASE_URL, useValue : API_CONFIG_BASE_URL.API_BASE_URL }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
